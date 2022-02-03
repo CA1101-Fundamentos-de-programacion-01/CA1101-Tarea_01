8ecb421325e87c80e041f434a62bed4aff6331b8
profe-kevs <98577482+profe-kevs@users.noreply.github.com>
2022-02-03 12:11:14 -0600

    Actualización de archivo de pruebas

---
+++ variables_test.py
@@ -0,0 +1,6 @@
+import variables;
+
+def test_variables():
+    assert 'monedas_encontradas' in dir(variables)
+    assert 'monedas_robadas' in dir(variables)
+    assert 'monedas_magicas' in dir(variables)
